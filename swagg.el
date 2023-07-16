ba538155927a1f5d6b310efd783647843362414c
Isa Mert Gurbuz <isamertgurbuz@gmail.com>
2023-07-16 23:14:14 +0200

    Update documentation

--- swagg.el
+++ swagg.el
@@ -24,7 +24,12 @@
 
 ;;; Commentary:
 
-;; A practical Emacs UI for Swagger definitions
+;; A practical Emacs UI for Swagger definitions.
+
+;; swagg.el lets you fetch and interact with OpenAPI (formerly
+;; Swagger) definitions.  You can think of it like the normal Swagger
+;; UI that your app has but works for multiple definitions and it's
+;; right inside your Emacs frame!
 
 ;;; Code:
 
