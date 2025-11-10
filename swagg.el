87bd1f698bc4c77a1c0d6b252e15f4ee345d2afa
Isa Mert Gurbuz <isamertgurbuz@gmail.com>
2025-11-10 21:34:58 +0100

    Bump version to 0.4.0

--- swagg.el
+++ swagg.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2023  Isa Mert Gurbuz
 
 ;; Author: Isa Mert Gurbuz <isamertgurbuz@gmail.com>
-;; Version: 0.3.2
+;; Version: 0.4.0
 ;; Homepage: https://github.com/isamert/swagg.el
 ;; License: GPL-3.0-or-later
 ;; Package-Requires: ((emacs "27.1") (compat "29.1.4.0") (request "0.3.3") (dash "2.19.1") (yaml "0.5.1") (s "1.13.1"))
